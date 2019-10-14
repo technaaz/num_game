d116e26350dca33df8f62e622359c387c389e181
technaaz <56102581+technaaz@users.noreply.github.com>
2019-10-14 10:40:58 +0530

    Add files via upload
    number winning game in python

---
+++ num_game.py
@@ -0,0 +1,21 @@
+import random
+wining_number = random.randint(1,100)
+guess = 1
+num = int(input("Guess number between 1 to 100: "))
+game_over = False
+while not game_over:
+    if num == wining_number:
+        print(f"You Won. Number gussed in {guess} attempt. ")
+        game_over = True
+    else:
+        if num < wining_number:
+            print("You Guess Too Low. Guess Again.")
+        else:
+            print("You Guess Too High. Guess Again.")
+
+        guess += 1
+        num = int(input("Guess Again: "))
+        if guess == 6:
+            game_over = True
+            print("You tried maximum attempt. Game Over")
+    
